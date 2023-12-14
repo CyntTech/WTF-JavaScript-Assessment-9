9e0dd6896cb7b77037dfa9a0e7d173ffedc08da5
Cynthia Amaji <cynthyok4luv@gmail.com>
2023-12-14 10:34:02 +0100

    function that prints value of array to the console

---
+++ wk-9-JavaScript-Assessment/printArray.js
@@ -0,0 +1,12 @@
+//function that prints the value of an array to the console
+
+function PrintArrayNumbers() {
+    const arrayNum = [0, 1, 3, 5, 7, 9, 11];
+    for (let i = 0; i < arrayNum.length; i++) {
+        console.log(arrayNum[i]);
+    }
+    return arrayNum;
+    
+}
+
+PrintArrayNumbers();
