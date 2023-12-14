1cdab48222e219658785f247f6ca02263a27077f
Cynthia Amaji <cynthyok4luv@gmail.com>
2023-12-14 10:33:20 +0100

    function that print numbers to the console

---
+++ wk-9-JavaScript-Assessment/printNumbers.js
@@ -0,0 +1,6 @@
+//function that print numbers to the console from 0 to the length of an array
+
+const printNumber = [0, 1, 3, 5, 7, 9, 11];
+for (let i = 0; i < printNumber.length; i++)
+console.log(printNumber[i]);
+
